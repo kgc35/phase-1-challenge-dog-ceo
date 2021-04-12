1be4291f32e9c75d76a58c177db6e050dec2da48
github username <github email address>
2021-04-12 15:00:52 -0400

    still working on changing color of breed list, Sean Ottomanelli, Kent Chan.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4" //added from lab
+const breedUrl = "https://dog.ceo/api/breeds/list/all" //added from lab
 
 //need to wait for content to load
 document.addEventListener("DOMContentLoaded", () => {
@@ -24,9 +25,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
             imageContainer.append(blankImg)
         })
-        debugger
-        
+        // debugger
+    })
+        //declare variable of location for dog breeds
 
+        let breedContainer = document.querySelector("ul#dog-breeds");
+        console.log(breedContainer)
+
+        // fetch the dog breeds
+
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then((breedPOJO) => {
+        console.log(breedPOJO)
+
+        breedArray = Object.keys(breedPOJO.message)
+        debugger
+
+   //append the dog breeds to the location
+
+   breedArray.forEach(element =>{
+   let blankLi = document.createElement("li")
+   blankLi.class = "breeds"
+   blankLi.innerText = element;
+   breedContainer.append(blankLi)
+})
     })
 
+     
+    function colorChange() {
+        console.log("This is test. we will make the color change.")
+    }
+    
+    breedListItems = document.querySelector("ul#dog-breeds")
+    
+    breedListItems.addEventListener("click",colorChange)
+    
+
 })
+
