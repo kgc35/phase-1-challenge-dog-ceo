9b1851e3e79cf6fc4187ac24ac9f2f774afc06b5
kent <kntchn@gmail.com>
2021-04-12 14:14:20 -0400

    214pm

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" //added from lab
+
+//need to wait for content to load
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then((responsePOJO) => {
+
+        let imageContainer = document.querySelector("div#dog-image-container");
+
+        console.log(responsePOJO)
+
+        //Now DOM Manipulation
+        dogArray = responsePOJO.message;
+
+
+
+        dogArray.forEach(element => {
+            // console.log(element)
+
+            let blankImg = document.createElement("img");
+            blankImg.src = element
+
+            imageContainer.append(blankImg)
+        })
+        debugger
+        
+
+    })
+
+})
